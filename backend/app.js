d7497e223dad02a04c62ddc2112e655fa3a5b96b
ChisvinAndrew1 <frolik947@gmail.com>
2022-06-23 02:36:08 +0300

    feat/backend3

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { celebrate, Joi, errors } = require('celebrate');
-const cors = require('cors');
+// const cors = require('cors');
 
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
@@ -15,9 +15,34 @@ app.use(express.json());
 app.use(cookieParser());
 mongoose.connect('mongodb://localhost:27017/mestodb');
 const { PORT = 3001 } = process.env;
-
+const allowedCors = [
+  'https://praktikum.tk',
+  'http://praktikum.tk',
+  'localhost:3000',
+  'http://localhost:3000',
+  'https://domainname.chisvin.nomoredomains.xyz',
+  'https://api.domainname.chisvin.nomoredomains.xyz',
+];
 console.log('ghsjkd');
-app.use(cors());
+// app.use(cors());
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  console.log(origin);
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
+  return null;
+});
 app.use(requestLogger);
 app.post('/signup', celebrate({
   body: Joi.object().keys({
